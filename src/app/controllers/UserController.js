3f157fb3ba41df24e46e4649a318be2a642a67cf
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-28 22:51:31 -0300

    Feature: password hash

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,17 @@
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 class UserController {
   async store(request, response) {
     const { name, email, password } = request.body;
 
+    const passwordHashed = await hash(password, 8);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: passwordHashed,
     });
 
     return response.json(user);
