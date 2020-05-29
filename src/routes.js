b2c8b830a9ea17b3cc1f5dd46b33c38070b94131
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-28 23:55:38 -0300

    Chore: applicating the auth middleware

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,14 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import MonitoringController from './app/controllers/MonitoringController';
 
+import auth from './app/middlewares/auth';
+
 const routes = Router();
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+routes.use(auth);
 
 routes.post('/monitoring', MonitoringController.store);
 
